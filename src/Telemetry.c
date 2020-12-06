d0601b735518ad48cd2b8dfc33a7f5fef9985788
harelevi <75210226+harelevi@users.noreply.github.com>
2020-12-06 16:47:25 +0200

    Update Telemetry.c

--- src/Telemetry.c
+++ src/Telemetry.c
@@ -4,5 +4,23 @@
  *  Created on: 6 בדצמ× 2020
  *      Author: Alihai
  */
+#include "utils.h"
+#include "errors.h"
 
 
+
+Boolean _flagTelemetryInit = FALSE;
+Boolean _flaghcc_mem_init = FALSE;
+
+
+
+int fs_init(){
+	_flagTelemetryInit = TRUE;
+	return	E_NO_SS_ERR ;
+}
+
+
+int hcc_mem_init(){
+	_flaghcc_mem_init = TRUE;
+	return	E_NO_SS_ERR ;
+}
