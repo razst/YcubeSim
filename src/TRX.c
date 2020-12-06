122fc94c1dc1690f87d88a293bad6c4375362a77
gelkopem <75211290+gelkopem@users.noreply.github.com>
2020-12-06 16:45:41 +0200

    added TRX init

--- src/TRX.c
+++ src/TRX.c
@@ -5,9 +5,21 @@
  *      Author: Emmanuel Gelkop
  */
 #include "TRX.h"
+#include "utils.h"
+#include "errors.h"
 
+// all local variables
+Boolean _initFlag=FALSE;
+ISIStrxvuFrameLengths _maxFrameLengths;
 
-int IsisTrxvu_initialize(ISIStrxvuI2CAddress *address, ISIStrxvuFrameLengths *maxFrameLengths, ISIStrxvuBitrate* default_bitrates, unsigned char number);
+int IsisTrxvu_initialize(ISIStrxvuI2CAddress *address, ISIStrxvuFrameLengths *maxFrameLengths, ISIStrxvuBitrate* default_bitrates, unsigned char number){
+	if(_initFlag) return E_IS_INITIALIZED;
+
+	_maxFrameLengths.maxAX25frameLengthRX = maxFrameLengths->maxAX25frameLengthRX;
+	_maxFrameLengths.maxAX25frameLengthTX = maxFrameLengths->maxAX25frameLengthTX;
+	_initFlag=TRUE;
+}
+
 
 
 
