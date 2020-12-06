4e55839fc163e8d09645c61ea0ba36757048019d
gelkopem <75211290+gelkopem@users.noreply.github.com>
2020-12-06 16:56:59 +0200

    added Ants init

--- src/TRX.c
+++ src/TRX.c
@@ -11,6 +11,8 @@
 // all local variables
 Boolean _initFlag=FALSE;
 ISIStrxvuFrameLengths _maxFrameLengths;
+Boolean _initAnts=FALSE;
+ISISantsI2Caddress _address;
 
 int IsisTrxvu_initialize(ISIStrxvuI2CAddress *address, ISIStrxvuFrameLengths *maxFrameLengths, ISIStrxvuBitrate* default_bitrates, unsigned char number){
 	if(_initFlag) return E_IS_INITIALIZED;
@@ -18,8 +20,16 @@ int IsisTrxvu_initialize(ISIStrxvuI2CAddress *address, ISIStrxvuFrameLengths *ma
 	_maxFrameLengths.maxAX25frameLengthRX = maxFrameLengths->maxAX25frameLengthRX;
 	_maxFrameLengths.maxAX25frameLengthTX = maxFrameLengths->maxAX25frameLengthTX;
 	_initFlag=TRUE;
+	return E_NO_SS_ERR;
 }
 
+int IsisAntS_initialize(ISISantsI2Caddress* address, unsigned char number){
+	if(_initAnts) return E_IS_INITIALIZED;
+	_address.addressSideA=address->addressSideA;
+	_address.addressSideB=address->addressSideB;
+	_initAnts=TRUE;
+	return E_NO_SS_ERR;
+}
 
 
 
