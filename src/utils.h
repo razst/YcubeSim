c15aa57f0a750c5e57b462a1adc178e809731b81
gelkopem <75211290+gelkopem@users.noreply.github.com>
2020-12-06 16:22:07 +0200

    added utils file

---
+++ src/utils.h
@@ -0,0 +1,17 @@
+/*
+ * utils.h
+ *
+ *  Created on: 6 בדצמ× 2020
+ *      Author: ruvig
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+typedef int Boolean;
+
+#define TRUE 0xFFFFFFFF
+#define FALSE 0x0
+
+
+#endif /* UTILS_H_ */
