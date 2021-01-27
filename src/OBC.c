351395dcd5138b2b8abd06b7ee60a74458612052
noamloev <77726445+noamloev@users.noreply.github.com>
2021-01-27 21:58:02 +0200

    יצירת קובץ והכנסת ערך ןהדפסת הקובץ

--- src/OBC.c
+++ src/OBC.c
@@ -19,7 +19,28 @@ int FRAM_start(void){
 int FRAM_write(const unsigned char *data, unsigned int address, unsigned int size){
 	if(_flagF_FRAM_start == TRUE){
 	//FARM_start did work
-		printf("it worked\n");
+		int num;
+		FILE *fptr;
+		fptr = fopen("C:\\program.txt","w");
+
+		if(fptr == NULL)
+		{
+	      printf("Error!");
+	      exit(1);
+		}
+
+		printf("Enter num: ");
+		scanf("%d",&num);
+		fprintf(fptr,"%d",num);
+		fclose(fptr);
+		if ((fptr = fopen("C:\\program.txt","r")) == NULL){
+			printf("Error! opening file");
+			exit(1);
+		}
+
+		fscanf(fptr,"%d", &num);
+		printf("Value of num = %d\n", num);
+		fclose(fptr);
 	}
 	else{
 	//FRAM_start didnt work
