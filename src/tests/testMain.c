99ec7ad35231625c99436729fadd8acc94065550
Raz <raz_st@hotmail.com>
2021-01-31 19:13:46 +0200

    run suite example

--- src/tests/testMain.c
+++ src/tests/testMain.c
@@ -16,14 +16,15 @@
 
 int runMainTests(void){
     CU_initialize_registry();
-    CU_pSuite suite = CU_add_suite("TRX Tests", 0, 0);
+    CU_pSuite suiteTX = CU_add_suite("TRX Tests", 0, 0);
 
-    CU_add_test(suite, "send message", testSendMeesage);
-    CU_add_test(suite, "trx init", testTrxInit);
+    CU_add_test(suiteTX, "send message", testSendMeesage);
+    CU_add_test(suiteTX, "trx init", testTrxInit);
 
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
+//    CU_basic_run_suite(suiteTX);
     CU_cleanup_registry();
     return 0;
 }
