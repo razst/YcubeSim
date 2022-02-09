5380910cf3fc18a68508ed1ffd701e9d1ef8ad0c
emailmeydrive <emailme.ydrive@gmail.com>
2022-02-09 20:00:38 +0200

    Update testEPS.c

--- src/tests/testEPS.c
+++ src/tests/testEPS.c
@@ -1,10 +1,20 @@
-
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
 #include "..\EPS.h"
 #include "..\errors.h"
 #include "testBasic.h"
 
 void testISIS_EPS_Init(){
+	ISIS_EPS_t i;
+	i.i2cAddr = 55; //"t";
+	uint8_t isis_epsCount = 55; //"t";
+	//55 is just a random value
+	int err = ISIS_EPS_Init(&i, isis_epsCount);
+	ASSERT_INT(err,E_NO_SS_ERR);
 
+	err = ISIS_EPS_Init(&i, isis_epsCount);
+	ASSERT_INT(err,E_IS_INITIALIZED);
 }
 
 void testIsisSolarPanelv2_initialize(){
