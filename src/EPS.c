d9ef05441de712f40525d08c49fa5955e0c38c32
gelkopem <75211290+gelkopem@users.noreply.github.com>
2022-03-15 18:23:56 +0200

    Update EPS.c

--- src/EPS.c
+++ src/EPS.c
@@ -11,15 +11,16 @@
 #define REG_RESET  0x00
 #define REG_DATA_ch1  0x01 //ch 1 shunt
 
-
+//ina 219
 ISIS_EPS_t _isis_eps;
 Boolean _flagEpsInit = FALSE;
 Boolean _flagSolarPanelInit = FALSE;
+
 //to ina3221
 int fd;
 
 
-
+//start ina219
 int ISIS_EPS_Init( ISIS_EPS_t* isis_eps, uint8_t isis_epsCount ){
 	if (_flagEpsInit == TRUE){
 		return E_IS_INITIALIZED;
@@ -28,14 +29,14 @@ int ISIS_EPS_Init( ISIS_EPS_t* isis_eps, uint8_t isis_epsCount ){
 		_isis_eps.i2cAddr = isis_eps->i2cAddr;
 		return E_NO_SS_ERR;
 }
-
+//
 IsisSolarPanelv2_State_t IsisSolarPanelv2_getState(){
 	if (_flagSolarPanelInit == FALSE){
 		return ISIS_SOLAR_PANEL_STATE_NOINIT;
 	}
 	return ISIS_SOLAR_PANEL_STATE_SLEEP;
 }
-
+//return state or error
 int IsisSolarPanelv2_initialize( SPIslave slave ){
 	if (_flagSolarPanelInit == TRUE){
 		return E_IS_INITIALIZED;
@@ -49,21 +50,21 @@ int IsisSolarPanelv2_initialize( SPIslave slave ){
 
 	 return IsisSolarPanelv2_getState();
 }
-
+//turn off solar panels
 int IsisSolarPanelv2_Dinitialize(){
 	 _flagSolarPanelInit = FALSE;
 	 return E_NO_SS_ERR;
 
 }
-
+//what return get in to the variable
 int isis_eps__gethousekeepingengincdb__tm( uint8_t index, isis_eps__gethousekeepingengincdb__from_t *response ){
 	response->fields.temp= (short) get_eps_temp();
-	response->fields.temp = get_eps_temp();
+	//response->fields.temp = get_eps_temp();
 	printf ("%d",response->fields.temp);
 	//need to return something
 	return 0;
 }
-
+//get temperature
 double get_eps_temp (){
 	FILE *temperatureFile;
 	double T;
@@ -77,6 +78,7 @@ double get_eps_temp (){
 	fclose (temperatureFile);
 	return T;
 }
+//end ina219
 
 //functions to 3221
 //the function are checking if the ina3221 is in position 0x40
@@ -103,6 +105,7 @@ int communication ()
 	return 0; //to change
 }
 
+//converting
 float shunt_to_Amp(int amp)
 {
 	if (amp > 4096){
@@ -123,8 +126,8 @@ unsigned int change_endian(unsigned int x)
     return ( (ptr[0] << 8) | ptr[1]);
 }
 
-//the function is getting the amp and present it
-int getamp(fd){
+//the function is getting the amper and present it
+float getamp(){
 	//read the shunt from file
     int shunt1 = wiringPiI2CReadReg16(fd, REG_DATA_ch1);
     //change endian, strip last 3 bits provide raw value
