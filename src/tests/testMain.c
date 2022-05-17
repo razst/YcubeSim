b88c21b10b27c452d00412cc09c8055806d10930
levhavayeda <89065326+levhavayeda@users.noreply.github.com>
2022-05-17 18:21:16 +0300

    Update testMain.c
    commenting for debug - TODO: return all tests

--- src/tests/testMain.c
+++ src/tests/testMain.c
@@ -24,36 +24,36 @@ int runMainTests(void){
 	testTRXInit();
 	testTRXSendMeesage();
 	printf("\n/*end testTRXSendMeesage */\n\n");
-	testIsisAntS_initialize();
-	testISIStrxvuIdleState();
-	printf("\n/*start TRX test Functions */\n\n");
-
-	printf("\n/* EPS test Functions */\n\n");
-	testISIS_EPS_Init();
-	testIsisSolarPanelv2_initialize();
-	testIsisSolarPanelv2_getState();
-	testIsisSolarPanelv2_Dinitialize();
-	//testGet_eps_temp ();
-	//testINA219();
-	printf("\n/* EPS test Functions */\n\n");
-
-	printf("\n/*start TLM test Functions */\n\n");
-	 testTLMfs_init();
-     testhcc_mem_init();
-     testf_enterFS();
-     //testf_initvolume();
-	printf("\n/*finish TLM test Functions */\n\n");
-
-
-	printf("\n/*Start OBC test Functions */\n\n");
-	testFRAMstart();
-	testFRAMwrite();
-	testFRAMread();
-	testTime_start();
-	testTime_setGetUnixEpoch();
-	testvTaskDelay();
-	finishTesting();
-	printf("\n/*Finish OBC test Functions */\n\n");
+//	testIsisAntS_initialize();
+//	testISIStrxvuIdleState();
+//	printf("\n/*start TRX test Functions */\n\n");
+//
+//	printf("\n/* EPS test Functions */\n\n");
+//	testISIS_EPS_Init();
+//	testIsisSolarPanelv2_initialize();
+//	testIsisSolarPanelv2_getState();
+//	testIsisSolarPanelv2_Dinitialize();
+//	//testGet_eps_temp ();
+//	//testINA219();
+//	printf("\n/* EPS test Functions */\n\n");
+//
+//	printf("\n/*start TLM test Functions */\n\n");
+//	 testTLMfs_init();
+//     testhcc_mem_init();
+//     testf_enterFS();
+//     //testf_initvolume();
+//	printf("\n/*finish TLM test Functions */\n\n");
+//
+//
+//	printf("\n/*Start OBC test Functions */\n\n");
+//	testFRAMstart();
+//	testFRAMwrite();
+//	testFRAMread();
+//	testTime_start();
+//	testTime_setGetUnixEpoch();
+//	testvTaskDelay();
+//	finishTesting();
+//	printf("\n/*Finish OBC test Functions */\n\n");
 
 	return 0;
 }
