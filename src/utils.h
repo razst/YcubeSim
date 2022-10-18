5e09dc8cb6aac26a722e6c737940f9c79b58b76d
levhavayeda <89065326+levhavayeda@users.noreply.github.com>
2022-10-18 18:56:18 +0300

    boolean
    8bit support

--- src/utils.h
+++ src/utils.h
@@ -8,11 +8,15 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
-typedef int Boolean;
+typedef unsigned int  Boolean;
+typedef unsigned char Boolean8bit;
 
 #define TRUE 0xFFFFFFFF
 #define FALSE 0x0
 #define NULL ((void *)0)
+
+#define TRUE_8BIT	0xFF
+#define FALSE_8BIT	0
 
 #endif /* UTILS_H_ */
 
