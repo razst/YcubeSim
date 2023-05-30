b1f50198b5b708c4322574f8f0a9b356b287211f
adanynave <127877123+adanynave@users.noreply.github.com>
2023-05-30 19:25:35 +0300

    edit initTrxvu func
    define addresses and frames length

--- src/ycube/TRXVU.c
+++ src/ycube/TRXVU.c
@@ -5,10 +5,21 @@
  *      Author: Magshimim
  */
 
-
+#include "..\sim\TRX.h"
 
 int initTrxvu()
 {
+	ISIStrxvuI2CAddress TRXVUaddress; //define address struct
+	ISIStrxvuFrameLengths TRXUVframeLength; //define frame size struct
+
+	//define addresses
+	TRXVUaddress.addressVu_rc = 0x1;
+	TRXVUaddress.addressVu_tc = 0x2;
+
+	//define frame sizes
+	TRXUVframeLength.maxAX25frameLengthRX = 100;
+	TRXUVframeLength.maxAX25frameLengthTX = 100;
+
 	return 0;
 }
 
