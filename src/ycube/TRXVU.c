ffe941cc4168efcfd16daf08b6a40835ed21868f
Regev_Shoham <80406171+regev-s@users.noreply.github.com>
2023-06-06 18:45:22 +0300

    Update TRXVU.c

--- src/ycube/TRXVU.c
+++ src/ycube/TRXVU.c
@@ -11,6 +11,8 @@ int initTrxvu()
 {
 	ISIStrxvuI2CAddress TRXVUaddress; //define address struct
 	ISIStrxvuFrameLengths TRXUVframeLength; //define frame size struct
+	ISISantsI2Caddress anttenaAdress;
+	ISIStrxvuBitrate TRXVUbitrates;
 
 	//define addresses
 	TRXVUaddress.addressVu_rc = 0x1;
@@ -19,6 +21,12 @@ int initTrxvu()
 	//define frame sizes
 	TRXUVframeLength.maxAX25frameLengthRX = 100;
 	TRXUVframeLength.maxAX25frameLengthTX = 100;
+
+	//define antennas addresses
+	anttenaAdress.addressSideA = 0x3;
+	anttenaAdress.addressSideB = 0x4;
+
+	TRXVUbitrates = trxvu_bitrate_9600;
 
 	return 0;
 }
